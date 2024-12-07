e5a8c4dfb60c82113a20494702cac8b4f21f5a98
KDavies2022 <keaton.davies@snhu.edu>
2024-12-07 15:01:17 -0600

    Module 6 complete baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -52,7 +52,78 @@ const tripsFindByCode= async (req, res) => {
     }
 };
 
+const tripsAddTrip = async(req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    const q = await newTrip.save();
+
+        if(!q)
+        {
+            return res
+                .status(400)
+                .json(err);
+        } else {
+            return res
+                .status(201)
+                .json(q);
+        }
+
+        //uncomment this to get the following console results
+        //console.log(q);
+};
+
+//PUT: /trips/:tripCode - Adds a new Trip
+//regardless of outcome, response must include HTML status code
+//and JSON message to requesting client
+const tripsUpdateTrip = async(req, res) => {
+
+    //Debugging
+    console.log(req.params);
+    console.log(req.body);
+
+    const q = await Model
+        .findOneAndUpdate(
+            {'code':req.params.tripCode},
+            {
+                code:req.body.code,
+                name:req.body.name,
+                length:req.body.length,
+                start:req.body.start,
+                resort:req.body.resort,
+                perPerson:req.body.perPerson,
+                image:req.body.image,
+                description:req.body.description
+            }
+        )
+        .exec()
+
+        if(!q)
+            {//Database returned nothing
+                return res
+                    .status(400)
+                    .json(err);
+            } else {
+                return res
+                    .status(201)
+                    .json(q);
+            }
+
+            //uncomment to show results
+            //console.log(q);
+        };
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };

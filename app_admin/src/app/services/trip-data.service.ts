fb5965cb11f198cdc67e2b9ae3729e940d483d2b
KDavies2022 <keaton.davies@snhu.edu>
2024-12-12 21:54:12 -0600

    Module 7 complete baseline

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,36 +1,51 @@
-import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { Injectable, Inject } from '@angular/core';
+import { Http } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Trip } from '../models/trip';
-
+import { User } from '../models/user';
+import { AuthResponse } from '../models/authresponse';
+import { BROWSER_STORAGE } from '../storage';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class TripDataService {
-
-  constructor(private http: HttpClient) {}
   url = 'http://localhost:3000/api/trips';
 
-  getTrips() : Observable<Trip[]> {
+  constructor(private http: Http, @Inject(BROWSER_STORAGE) private storage: Storage) {}
+
+  getTrips(): Observable<Trip[]> {
     return this.http.get<Trip[]>(this.url);
   }
 
-  addTrip(formData: Trip) : Observable<Trip>{
+  addTrip(formData: Trip): Observable<Trip> {
     return this.http.post<Trip>(this.url, formData);
   }
 
-  getTrip(tripCode: string) : Observable<Trip[]> {
-    //console.log('Inside TripDataService::getTrips');
-    return this.http.get<Trip[]>(this.url + '/' + tripCode);
+  getTrip(tripCode: string): Observable<Trip[]> {
+    return this.http.get<Trip[]>(`${this.url}/${tripCode}`);
   }
 
-  updateTrip(formData: Trip) : Observable<Trip> {
-    //console.log('Inside TripDataService::addTrips');
-    return this.http.put<Trip>(this.url + '/' + formData.code, formData);
+  updateTrip(formData: Trip): Observable<Trip> {
+    return this.http.put<Trip>(`${this.url}/${formData.code}`, formData);
   }
-  
+
+  public login(user: User): Promise<AuthResponse> {
+    return this.makeAuthApiCall('login', user);
+  }
+
+  public register(user: User): Promise<AuthResponse> {
+    return this.makeAuthApiCall('register', user);
+  }
+
+  private makeAuthApiCall(urlPath: string, user: User): Promise<AuthResponse> {
+    const url: string = `${this.url}/${urlPath}`;
+    return this.http
+      .post(url, user)
+      .toPromise()
+      .then(response => response.json() as AuthResponse)
+      .catch(this.handleError);
+  }
+
 }
-
--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-card',
@@ -14,15 +15,17 @@ export class TripCardComponent implements OnInit {
 
   @Input('trip') trip: any;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private AuthenticationService: AuthenticationService) {}
 
   ngOnInit() : void{
 
   }
 
-  public editTrip(trip: Trip){
-    localStorage.removeItem('tripCode');
-    localStorage.setItem('tripCode', trip.code);
-    this.router.navigate(['edit-trip']);
+  public isLoggedIn(): boolean {
+    return this.AuthenticationService.isLoggedIn();
+  }
+  
+  private editTrip(trip:Trip): void {
+    console.log('Inside TripListingComponent#editTrip');
   }
 }
